153b9e298f6e92c1257d25136551653a64398726
Ferenc Kondasz <ferenc@getbynder.com>
2017-03-16 19:16:20 +0100

    rollback

--- superlist/settings.py
+++ superlist/settings.py
@@ -101,7 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/../static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/../static')
-
-print('XXXXXXXXXX')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
